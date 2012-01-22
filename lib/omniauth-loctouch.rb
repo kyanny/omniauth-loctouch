e90929477b02f551546e8de0a7b4cb3a69343c0f
Kensuke Nagae <kyanny@gmail.com>
2012-01-22 22:58:42 +0900

    initial commit

---
+++ lib/omniauth-loctouch.rb
@@ -0,0 +1,7 @@
+require "omniauth-loctouch/version"
+
+module Omniauth
+  module Loctouch
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth-loctouch/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Loctouch
+    VERSION = "0.0.1"
+  end
+end
